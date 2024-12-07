ac6a8ed70fb7bedda6248ef5a0b0419e896d2656
borisoid <boris.lazarev90@mail.ru>
2024-12-07 20:28:41 +0300

    Add `test_pyright`

--- tests/model/test_types.py
+++ tests/model/test_types.py
@@ -1,10 +1,16 @@
 import subprocess
+import sys
 from pathlib import Path
 
 
 def test_mypy() -> None:
-    subprocess.check_call(["mypy", Path.cwd()])
+    import mypy.api
+
+    stdout, stderr, code = mypy.api.run([str(Path.cwd())])
+    print(stdout)
+    print(stderr)
+    assert code == 0
 
 
-# def test_pyright() -> None:
-#     subprocess.check_call(["pyright", "."])
+def test_pyright() -> None:
+    subprocess.check_call(["pyright", ".", "--pythonpath", sys.executable])
